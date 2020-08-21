f26fac648d976ea142fd5756fd27d0bc45f9710f
dimapanasiuk <dimapanasiuk@icloud.com>
2020-08-21 16:05:11 +0300

    init: initial fake server with help json server

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import MainLayout from "../components/MainLayout";
 
-const Home: React.FC = () => {
+const Home: React.FC = ({ postsData }) => {
+  const [posts, setPosts] = useState([]);
   return (
     <>
       <MainLayout title="test">
         <h1>check app</h1>
+        <ul>
+          {postsData.map((i) => (
+            <li key={i.id}>{i.title}</li>
+          ))}
+        </ul>
       </MainLayout>
     </>
   );
+};
+
+Home.getInitialProps = async () => {
+  const res = await fetch("http://localhost:4000/posts");
+  const json = await res.json();
+  return { postsData: json };
 };
 
 export default Home;
