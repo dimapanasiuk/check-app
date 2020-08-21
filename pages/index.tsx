8b1af384c8d29d8ea395139dfd9e4fa5af087abe
dimapanasiuk <dimapanasiuk@icloud.com>
2020-08-21 16:47:21 +0300

    feat: implementation local db and install axios for work with post requests

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import MainLayout from "../components/MainLayout";
 
-const Home: React.FC = ({ postsData }) => {
+const Home: React.FC = () => {
   const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // TODO: panasiuk use Home.getInitialProps instead of useEffect for SEO
+    axios
+      .get(`http://localhost:4000/posts`)
+      .then((data) => setPosts(data.data));
+  }, []);
+
+  const testAxios: void = ()  => {
+    axios
+      .post("http://localhost:4000/posts", {
+        id: "your id",
+        title: "your tittle",
+        body: "your body",
+      })
+      .then(function (response) {
+        console.log("response", response);
+      })
+      .catch(function (error) {
+        console.log("error", error);
+      });
+  };
+
   return (
     <>
-      <MainLayout title="test">
-        <h1>check app</h1>
+      <MainLayout title="home page">
+        <h1>Home page</h1>
         <ul>
-          {postsData.map((i) => (
+          {posts.map((i) => (
             <li key={i.id}>{i.title}</li>
           ))}
         </ul>
+        <button onClick={testAxios}>testAxios</button>
       </MainLayout>
     </>
   );
-};
-
-Home.getInitialProps = async () => {
-  const res = await fetch("http://localhost:4000/posts");
-  const json = await res.json();
-  return { postsData: json };
 };
 
 export default Home;
