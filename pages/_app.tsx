f145b826113ec722524021d8d1bd8a19fed0d4ad
dimapanasiuk <dimapanasiuk@icloud.com>
2020-08-25 10:23:55 +0300

    init: start work with apollo and graph ql

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 import "antd/dist/antd.css";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  cache: new InMemoryCache(),
+});
 
 const MyApp: React.FC = ({ Component, pageProps }: any) => {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />;
+    </ApolloProvider>
+  );
 };
 
 export default MyApp;
