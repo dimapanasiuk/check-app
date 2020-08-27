0414f33e3f4933be360a952e48d2e1d4cb763cfb
gordeyst <gordeystahovets@gmail.com>
2020-08-27 20:33:06 +0300

    refactor: little styles refactor

--- components/createTask/CheckTask.tsx
+++ components/createTask/CheckTask.tsx
@@ -16,19 +16,21 @@ const CheckTask: React.FC<ICheckTask> = ({
 }) => {
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>Check Task</h1>
-      <h1 style={{ textAlign: "center" }}>Your task name: {taskName}</h1>
-      <h1 style={{ textAlign: "center" }}>Your maximum score: {maxScore} </h1>
-      {taskDescription && (
-        <React.Fragment>
-          <h1>Task description:</h1>
-          <h1 style={{ textAlign: "center", maxWidth: "800px" }}>
-            {taskDescription}
-          </h1>
-        </React.Fragment>
-      )}
+      <div style={{ textAlign: "center" }}>
+        <h1>Check Task</h1>
+        <h1>Your task name: {taskName}</h1>
+        <h1>Your maximum score: {maxScore} </h1>
+        {taskDescription && (
+          <React.Fragment>
+            <h1>Task description:</h1>
+            <h1 style={{ margin: "0 auto", maxWidth: "1000px" }}>
+              {taskDescription}
+            </h1>
+          </React.Fragment>
+        )}
 
-      {rdmd(rmBody)}
+        {rdmd(rmBody)}
+      </div>
     </>
   );
 };
