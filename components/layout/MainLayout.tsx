7775f2faf4adb2ee5e353a3c69e01c8f962d07a5
burik84-home <avburik84@gmail.com>
2020-09-10 01:58:36 +0300

    feat: add class and styles container for login

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -29,103 +29,105 @@ const MainLayout: React.FC<IMainLayout> = ({
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Layout style={{ minHeight: "100vh" }}>
-        <Header className={styles.header}>
-          <Link href="/">
-            <a>
-              <Title mark level={2} className={styles.logo}>
-                RS School
-              </Title>
-            </a>
-          </Link>
-
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            // defaultSelectedKeys={["1","2","3"]}
-            className={styles.links}
-          >
-            {(() => {
-              if (role === "admin" || role === "student") {
-                return (
-                  <Menu.Item key="1">
-                    <Link href="/">
-                      <a>cabinet</a>
-                    </Link>
-                  </Menu.Item>
-                );
-              }
-            })()}
-            {(() => {
-              if (role === "mentor" || role === "admin") {
-                return (
-                  <Menu.Item key="2">
-                    <Link href="/create">
-                      <a>create task</a>
-                    </Link>
-                  </Menu.Item>
-                );
-              }
-            })()}
-            {(() => {
-              if (role === "admin" || role === "mentor" || role === "student") {
-                return (
-                  <Menu.Item key="3">
-                    <Link href="/tasks">
-                      <a>tasks</a>
-                    </Link>
-                  </Menu.Item>
-                );
-              }
-            })()}
-            {(() => {
-              if (role === "admin" || role === "mentor" || role === "student") {
-                return (
-                  <Menu.Item key="4">
-                    <Link href="/review">
-                      <a>review</a>
-                    </Link>
-                  </Menu.Item>
-                );
-              }
-            })()}
-            {(() => {
-              if (role === "admin" || role === "student") {
-                return (
-                  <Menu.Item key="5">
-                    <Link href="/dashboard">
-                      <a>dashboard</a>
-                    </Link>
-                  </Menu.Item>
-                );
-              }
-            })()}
-            {(() => {
-              if (role === "mentor" || role === "student" || role === "admin") {
-                return (
-                  <Menu.Item key="6">
-                    <Link href="/score">
-                      <a>score</a>
-                    </Link>
-                  </Menu.Item>
-                );
-              }
-            })()}
-          </Menu>
-
-          <div className={styles.oauth}>
-            <Link href="/login">
+        <Header >
+          <div className={`${styles.container} ${styles.header}`}>
+            <Link href="/">
               <a>
-                <Button type="primary" icon={<LoginOutlined />} size="large">
-                  Login
-                </Button>
+                <Title mark level={2} className={styles.logo}>
+                  RS School
+                </Title>
               </a>
             </Link>
+
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              // defaultSelectedKeys={["1","2","3"]}
+              className={styles.links}
+            >
+              {(() => {
+                if (role === "admin" || role === "student") {
+                  return (
+                    <Menu.Item key="1">
+                      <Link href="/">
+                        <a>cabinet</a>
+                      </Link>
+                    </Menu.Item>
+                  );
+                }
+              })()}
+              {(() => {
+                if (role === "mentor" || role === "admin") {
+                  return (
+                    <Menu.Item key="2">
+                      <Link href="/create">
+                        <a>create task</a>
+                      </Link>
+                    </Menu.Item>
+                  );
+                }
+              })()}
+              {(() => {
+                if (role === "admin" || role === "mentor" || role === "student") {
+                  return (
+                    <Menu.Item key="3">
+                      <Link href="/tasks">
+                        <a>tasks</a>
+                      </Link>
+                    </Menu.Item>
+                  );
+                }
+              })()}
+              {(() => {
+                if (role === "admin" || role === "mentor" || role === "student") {
+                  return (
+                    <Menu.Item key="4">
+                      <Link href="/review">
+                        <a>review</a>
+                      </Link>
+                    </Menu.Item>
+                  );
+                }
+              })()}
+              {(() => {
+                if (role === "admin" || role === "student") {
+                  return (
+                    <Menu.Item key="5">
+                      <Link href="/dashboard">
+                        <a>dashboard</a>
+                      </Link>
+                    </Menu.Item>
+                  );
+                }
+              })()}
+              {(() => {
+                if (role === "mentor" || role === "student" || role === "admin") {
+                  return (
+                    <Menu.Item key="6">
+                      <Link href="/score">
+                        <a>score</a>
+                      </Link>
+                    </Menu.Item>
+                  );
+                }
+              })()}
+            </Menu>
+
+            <div className={styles.oauth}>
+              <Link href="/login">
+                <a>
+                  <Button type="primary" icon={<LoginOutlined />} size="large">
+                    Login
+                  </Button>
+                </a>
+              </Link>
+            </div>
           </div>
         </Header>
         <Content style={{ padding: "0 50px" }}>
-          <div className="site-layout-content">{children}</div>
+          <div className={`site-layout-content ${styles.container}`} >{children}</div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
+        <Footer style={{ textAlign: "center" }} className={styles.container}>
           Ant Design ©2018 Created by Ant UED
         </Footer>
       </Layout>
