8eba6a5b9d55724268beb0437d17b608c28e75b2
burik84-home <avburik84@gmail.com>
2020-09-16 00:26:05 +0300

    feat: pages 404 final

---
+++ pages/404.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from "next/link";
+import { Result, Button } from 'antd';
+
+
+const ErrorPage404=()=>{
+    return (
+        <Result
+            status="404"
+            title="404"
+            subTitle="Sorry, the page you visited does not exist."
+            extra={<Button type="primary">
+                <Link href="/">
+                    <a>Back Home</a>
+                </Link>
+            </Button>} />
+    );
+}
+
+export default ErrorPage404;
