49191842b2499c0a86b89c51fc4d84349110d940
dimapanasiuk <dimapanasiuk@icloud.com>
2020-09-16 11:21:00 +0300

    refactor: refactoring 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React from "react";
 import Link from "next/link";
-import { Result, Button } from 'antd';
+import { Result, Button } from "antd";
 
-
-const ErrorPage404=()=>{
-    return (
-        <Result
-            status="404"
-            title="404"
-            subTitle="Sorry, the page you visited does not exist."
-            extra={<Button type="primary">
-                <Link href="/">
-                    <a>Back Home</a>
-                </Link>
-            </Button>} />
-    );
-}
+const ErrorPage404: React.FC = () => {
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button type="primary">
+          <Link href="/">
+            <a>Back Home</a>
+          </Link>
+        </Button>
+      }
+    />
+  );
+};
 
 export default ErrorPage404;
